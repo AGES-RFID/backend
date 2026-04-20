4e675be873d1f20cfca143ea89068a05a144742d
bepaiva <b.hoff@edu.pucrs.br>
2026-04-20 17:23:06 -0300

    chore: deleted unnecessary migrations

--- Backend.API/Database/Migrations/20260418051710_TransactionTable.cs
+++ Backend.API/Database/Migrations/20260418051710_TransactionTable.cs
@@ -11,21 +11,23 @@ namespace Backend.Database.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql("CREATE TYPE transaction_type AS ENUM ('DEPOSIT', 'WITHDRAWAL');");
+
             migrationBuilder.CreateTable(
                 name: "transactions",
                 columns: table => new
                 {
-                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    transaction_id = table.Column<Guid>(type: "uuid", nullable: false),
                     user_id = table.Column<Guid>(type: "uuid", nullable: false),
                     amount = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
                     created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
                     updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
                     description = table.Column<string>(type: "text", nullable: false),
-                    transaction_type = table.Column<int>(type: "transaction_type", nullable: false)
+                    transaction_type = table.Column<string>(type: "transaction_type", nullable: false, defaultValue: "DEPOSIT")
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("pk_transactions", x => x.id);
+                    table.PrimaryKey("pk_transactions", x => x.transaction_id);
                 });
         }
 
@@ -34,6 +36,8 @@ namespace Backend.Database.Migrations
         {
             migrationBuilder.DropTable(
                 name: "transactions");
+
+            migrationBuilder.Sql("DROP TYPE IF EXISTS transaction_type;");
         }
     }
 }
